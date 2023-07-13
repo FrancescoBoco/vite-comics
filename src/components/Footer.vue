12ca18ff5381df68a6c4e352f58b8f769bf09e15
FrancescoBoco <francesco.bocoux@gmail.com>
2023-07-13 18:19:42 +0200

    END top footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,86 @@
 </script>
 
 <template>
+<footer>
+    <div class="footer-top">
+        <div>
+            <div>
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <div>
+                    DIGITAL COMICS
+                </div>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <div>
+                    DC MERCHANDISE 
+                </div>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-subscriptions.png"  alt="">
+                <div>
+                    SUBSCRIPTION
+                </div>
+            </div>
+            <div>
+                <img src="../assets/img/buy-comics-shop-locator.png"  alt="">
+                <div>
+                   COMIC SHOP LOCATOR
+                </div>
+            </div>
+            <div>
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <div>
+                    DC POWER VISA
+                </div>
+            </div>
+           
+        </div>
+    </div>
 
+
+    <div class="footer-main">
+        <div class="container-footer-main">
+            
+        </div>
+    </div>
+
+
+    <div class="footer-bottom">
+        <div class="container-footer-bottom">
+
+
+        </div>
+    </div>
+
+</footer>
 
 </template>
 
 
-<style>
-
+<style lang="scss" scoped>
+.footer-top{
+    height: 200px;
+    background-color: #3382F1;
+    > div{
+        max-width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-around;
+        height: 100%;
+        > div {
+            height: 100%;
+            align-items: center;
+            display: flex;
+            img{
+                height: 70px;
+            }
+            > div{
+                padding: 10px;
+                color: white;
+                font-size: 0.9em;
+            }
+        }
+    }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,10 +4,25 @@
 
 <template>
 
-
+<body>
+    <div class="container">
+        <div>
+        --- CONTENT GOES HERE ---
+        </div>
+    </div>
+</body>
 </template>
 
 
-<style>
-
+<style lang="scss" scoped>
+.container{
+    background-color: black;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    div{
+        color: white;
+    }
+}
 </style>
