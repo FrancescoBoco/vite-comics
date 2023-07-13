1345942e142a100605125509037338977e774858
FrancescoBoco <francesco.bocoux@gmail.com>
2023-07-13 17:39:02 +0200

    END header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,121 @@
 </script>
 
 <template>
+<div class="header-empty">
 
+</div>
+<div class="container">
+   
+    <div class="header-components">
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <div class="elements" >
+                <ul>
+                    <li>
+                       <a href="#">
+                        CHARACTERS
+                       </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            COMICS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            MUVIES
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            TV
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            GAMES
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            COLLECTIBLES
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            VIDEOS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            FANS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            NEWS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            SHOP
+                        </a>
+                    </li>
+                    
+                </ul>
+        </div>
+    </div>
+</div>
 
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+.header-empty{
+    height: 80px;
+    background: rgb(0,0,0);
+    background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(85,85,85,1) 100%);
+}
+.container{
+    height: 100px;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.header-components{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    background-color: white;
+    
+}
+
+
+.elements{
+    display: flex;
+    width: 60%;
+}
+ul{
+    display: flex;
+    justify-content: space-around;
+    width: 100%
+}
+.elements ul li,
+.elements ul li a
+{
+    list-style: none;
+    text-decoration: none;
+    color: black;
+    font-size: 0.9em;
+    font-weight: bold;
+    
+}
+
+
+
+
 
 </style>
